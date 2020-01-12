e66607de84ad0700c13fe7ac61b05383a98e63ed
Ben Cessa <ben@pixative.com>
2020-01-12 12:30:53 -0500

    Skip dev document resolve on testing

--- resolver/main_test.go
+++ resolver/main_test.go
@@ -6,15 +6,16 @@ import (
 )
 
 func TestGet(t *testing.T)  {
-	t.Run("stack", func(t *testing.T) {
-		contents, err := Get("did:stack:v0:15gxXgJyT5tM5A4Cbx99nwccynHYsBouzr-0")
+	t.Run("bryk", func(t *testing.T) {
+		t.Skip()
+		contents, err := Get("did:bryk:7889c965-4644-44ff-b760-f396f1d11444")
 		if err != nil {
 			t.Fatal(err)
 		}
 		log.Printf("%s", contents)
 	})
-	t.Run("bryk", func(t *testing.T) {
-		contents, err := Get("did:bryk:7889c965-4644-44ff-b760-f396f1d11444")
+	t.Run("stack", func(t *testing.T) {
+		contents, err := Get("did:stack:v0:15gxXgJyT5tM5A4Cbx99nwccynHYsBouzr-0")
 		if err != nil {
 			t.Fatal(err)
 		}
